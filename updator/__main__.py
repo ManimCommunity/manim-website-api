883817f1b22df0c035ce5bfa3d541191142602a5
Naveen M K <naveen521kk@gmail.com>
2023-06-10 14:07:33 +0530

    updator: use standard sql query

--- updator/__main__.py
+++ updator/__main__.py
@@ -6,22 +6,18 @@ from datetime import datetime
 
 import feedparser
 import requests
-from sqlalchemy.dialects.mysql import insert
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # from ..api import db, app
 from api._config import *
 from api._crud import query_video
-from .helper import (get_youtube_channel_id_from_custom_name, is_manim_video,
-                     sanitize)
+from .helper import get_youtube_channel_id_from_custom_name, is_manim_video, sanitize
 from api._tables import video_table
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URI,
-    **SQLALCHEMY_ENGINE_OPTIONS
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URI, **SQLALCHEMY_ENGINE_OPTIONS)
 Session = sessionmaker(engine)
+
 
 def scrape_rss_feeds():
     # Get the README content
@@ -86,23 +82,41 @@ def scrape_rss_feeds():
             )
 
         for row in rows:
+            print("Video ID: ",row["yt_videoid"])
             try:
                 current_time = datetime.now()
-                insert_stmt = insert(video_table).values(
-                    {"created_at": current_time, **row}
-                )
-                on_duplicate_key_stmt = insert_stmt.on_duplicate_key_update(
-                    updated_at=current_time, **row
-                )
+                data = {"created_at": current_time, **row}
                 with Session() as session:
-                    response = session.execute(on_duplicate_key_stmt)
+                    existing_item = (
+                        session.query(video_table)
+                        .filter_by(yt_videoid=data["yt_videoid"])
+                        .first()
+                    )
+                    if existing_item:
+                        update_stmt = (
+                            video_table.update()
+                            .where(video_table.c.yt_videoid == data["yt_videoid"])
+                            .values(
+                                **{
+                                    **data,
+                                    "updated_at": current_time,
+                                }
+                            )
+                        )
+                        session.execute(update_stmt)
+                    else:
+                        ins_stmt = video_table.insert().values(**data)
+                        session.execute(ins_stmt)
+
                     session.commit()
             except:
                 logging.warning(f"Could not insert row: {row}")
                 print(traceback.format_exc())
+                return
     # Reset the cache
     query_video.cache_clear()
     return None
+
 
 if __name__ == "__main__":
     scrape_rss_feeds()
