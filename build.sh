d79618ccc11bc269fd8deac5ebeb6ce5dea74a15
Naveen M K <naveen521kk@gmail.com>
2025-02-01 12:23:36 +0000

    build.sh: update to use with uv
    and remove requirements.txt

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e
 
-poetry export -f requirements.txt --without-hashes -o requirements.txt
+# Install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# Add $HOME/.local/bin to PATH
+export PATH="$HOME/.local/bin:$PATH"
+
+# Generate requirements.txt
+uv export --format requirements-txt --no-hashes > requirements.txt
 
 mkdir -p public/
 cp requirements.txt public/requirements.txt
